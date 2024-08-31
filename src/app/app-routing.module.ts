04838735bb1db81ef0c892a9879e4880ba303265
hameedrana <0hameedrana@gmail.com>
2024-08-31 15:46:51 +0500

    trade mark UI design

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
+
+const routes: Routes = [
+  { path: 'login', component: LogInComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
